0ae45f361aa5169fcbf5a7d8c2d69f081b6acb1b
542113578@qq.com <542113578@qq.com>
2015-08-07 18:05:04 +0800

    update

--- pages/Index.js
+++ pages/Index.js
@@ -1,16 +1,14 @@
 var React = require('react-native');
-var {AppRegistry, ScrollView, TouchableHighlight, Text } = React;
+var { ScrollView, TouchableHighlight, Text } = React;
 
 var TouchDemo = React.createClass({
   render: function() {
     return (
       <ScrollView>
         <TouchableHighlight onPress={() => console.log('pressed')}>
-            <Text>Testerhome</Text>
+          <Text>Proper Touch Handling</Text>
         </TouchableHighlight>
       </ScrollView>
     );
   },
 });
-
-AppRegistry.registerComponent('HelloWorld', () => TouchDemo);
